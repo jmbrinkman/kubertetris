69e2a3b3e921de3097e6d6f724d68e76458440c3
Michiel Brinkman <Michiel.Brinkman@rackspace.com>
2019-10-20 22:49:45 +0200

    moo

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ clean = require("gulp-clean"),
 uglify = require("gulp-uglify"),
 concat = require("gulp-concat"),
 header = require("gulp-header"),
-zip = require("gulp-zip"),
 runSequence = require('run-sequence');
  
 var getVersion = function () {
@@ -16,7 +15,7 @@ var getCopyright = function () {
 };
 
 gulp.task('js', function () {
-    return gulp.src(['./src/assets/js/blockrain.jquery.libs.js', './src/assets/js/blockrain.jquery.src.js'])
+    return gulp.src(['./assets/js/blockrain.jquery.libs.js', './assets/js/blockrain.jquery.src.js'])
     .pipe(concat('blockrain.jquery.js'))
     .pipe(header(getCopyright(), {version: getVersion()}))
     .pipe(gulp.dest('./dist/assets/js'))
@@ -27,7 +26,7 @@ gulp.task('js', function () {
 });
 
 gulp.task('firebase', function () {
-    return gulp.src(['./src/assets/js/firebase.config.js'])
+    return gulp.src(['./assets/js/firebase.config.js'])
     .pipe(concat('firebase.config.js'))
     .pipe(gulp.dest('./dist/assets/js'))
     .pipe(uglify({preserveComments:'none'}))
@@ -36,27 +35,27 @@ gulp.task('firebase', function () {
 });
 
 gulp.task('css', function () {
-    return gulp.src(['./src/assets/css/blockrain.css'])
+    return gulp.src(['./assets/css/blockrain.css'])
     .pipe(gulp.dest('./dist/assets/css/'));
 });
 
 gulp.task('blocks', function () {
-    return gulp.src(['./src/assets/blocks/custom/*.*'])
+    return gulp.src(['./assets/blocks/custom/*.*'])
     .pipe(gulp.dest('./dist/assets/blocks/custom'));
 });
 
 gulp.task('html', function () {
-    return gulp.src(['./src/*.*'])
+    return gulp.src(['./*.*'])
     .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('images', function () {
-    return gulp.src(['./src/assets/images/*.*'])
+    return gulp.src(['./assets/images/*.*'])
     .pipe(gulp.dest('./dist/assets/images'));
 });
 
 gulp.task('audio', function () {
-    return gulp.src(['./src/assets/audio/*.*'])
+    return gulp.src(['./assets/audio/*.*'])
     .pipe(gulp.dest('./dist/assets/audio'));
 });
 
@@ -70,16 +69,9 @@ gulp.task('clean', function () {
     .pipe(clean());
 });
 
-gulp.task('dist', function () {
-    return gulp.src(['./dist/**/*.*'])
-    .pipe(zip('blockrain.zip'))
-    .pipe(gulp.dest('./dist'));
-});
-
-
 gulp.task('build', function(callback){
   runSequence('clean', 
-              'js','firebase','css', 'blocks', 'readme', 'dist', 'html', 'images', 'audio', 
+              'js','firebase','css', 'blocks', 'readme', 'html', 'images', 'audio', 
               callback);
 });
 
