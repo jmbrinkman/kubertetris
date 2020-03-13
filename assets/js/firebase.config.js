c21c06122e5477c39202b47554944b940bd34390
Michiel Brinkman <Michiel.Brinkman@rackspace.com>
2020-03-13 22:56:11 +0100

    moo

--- assets/js/firebase.config.js
+++ assets/js/firebase.config.js
@@ -1,11 +1,11 @@
 var firebaseConfig = {
-  apiKey: "AIzaSyCfe0CquH6951zj45yIW4p30j4Q7_AmSKU",
-  authDomain: "kubertetris.firebaseapp.com",
-  databaseURL: "https://kubertetris.firebaseio.com",
-  projectId: "kubertetris",
-  storageBucket: "kubertetris.appspot.com",
-  messagingSenderId: "282892629213",
-  appId: "1:282892629213:web:eb876f4a449f81bb975734",
+  apiKey: "AIzaSyCiNEzg2bndiZUvjBukD9aY65yKxmdfjtE",
+  authDomain: "mgcp-kubetetris-next.firebaseapp.com",
+  databaseURL: "https://mgcp-kubetetris-next.firebaseio.com",
+  projectId: "mgcp-kubetetris-next",
+  storageBucket: "mgcp-kubetetris-next.appspot.com",
+  messagingSenderId: "550594135167",
+  appId: "1:550594135167:web:ade9707f1705850817eb4a",
   measurementId: "G-CG6H5JDQ02"
 };
 firebase.initializeApp(firebaseConfig);
