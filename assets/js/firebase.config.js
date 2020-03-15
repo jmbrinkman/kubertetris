20563615a3531d2bb010483a36267037a25c101d
Michiel Brinkman <Michiel.Brinkman@rackspace.com>
2020-03-15 23:02:25 +0100

    moo

--- assets/js/firebase.config.js
+++ assets/js/firebase.config.js
@@ -3,7 +3,7 @@ var firebaseConfig = {
   authDomain: "mgcp-kubetetris-next.firebaseapp.com",
   databaseURL: "https://mgcp-kubetetris-next.firebaseio.com",
   projectId: "mgcp-kubetetris-next",
-  storageBucket: "mgcp-kubetetris-next.appspot.com",
+  //storageBucket: "mgcp-kubetetris-next.appspot.com",
   messagingSenderId: "550594135167",
   appId: "1:550594135167:web:ade9707f1705850817eb4a",
   measurementId: "G-CG6H5JDQ02"
